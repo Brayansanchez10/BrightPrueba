6393a25a86942063c374500b82d5eb185810e9d5
Juansitoh <juanfergamer77@gmail.com>
2024-10-11 11:53:04 -0500

    changes

--- src/api/user/permissions.request.js
+++ src/api/user/permissions.request.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-const api = 'http://localhost:3068/PE'
+const api = `https://apibrightmind.mesadoko.com/PE`
 
 export const getAllPermissions = () => axios.get(`${api}/permissions/getPermissions`)
