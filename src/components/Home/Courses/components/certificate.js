2f7aa8d2e935a20065039a4655ac97a7b32b2e86
Brayansanchez10 <sanchezcortesbrayan2003@gmail.com>
2024-12-09 08:15:31 -0500

    Ajustes cerficado y otras funcionalidades

--- src/components/Home/Courses/components/certificate.js
+++ src/components/Home/Courses/components/certificate.js
@@ -12,91 +12,65 @@ export const generatePremiumCertificatePDF = (username, documentNumber, courseTi
  doc.setFillColor(255, 255, 255);
  doc.rect(0, 0, 28, 21.6, "F");
 
- // Imágenes decorativas
- if (izquierdaarriba) {
-  doc.addImage(izquierdaarriba, "PNG", -11, -2, 30, 34);
-}
-if (derechaabajo) {
-  doc.addImage(derechaabajo, "PNG", 9, -8, 30, 36.2);
- }
+ // Imagenes A
+ if (estructura) {
+  doc.addImage(estructura, "JPEG", 0, -0, 28, 23)
+  }
+  // Imágenes decorativas
+  if (izquierdaarriba) {
+    doc.addImage(izquierdaarriba, "JPEG", -9, -3, 25, 20);
+  }
+  if (derechaabajo) {
+    doc.addImage(derechaabajo, "JPEG", 12.5, 5, 25, 20);
+  }
  // Configuración de fuentes
  doc.setFont("Arial", "normal");
 
  // Título principal
+ doc.setFont("times", "bold");
  doc.setTextColor(0, 0, 0);
- doc.setFontSize(70);
- doc.text("BRIGHTMIND", 14, 7, { align: "center" });
-
- // Subtítulo
  doc.setFontSize(25);
- doc.text("HACE CONSTANCIA A", 14, 8, { align: "center" });
+ doc.text("BRIGHTMIND", 14, 4.5, { align: "center" });
+
+ // Título segundario
+ doc.setFont("times", "bold");
+ doc.setTextColor(0, 0, 0);
+ doc.setFontSize(25);
+ doc.text("CONSTANCIA DE APRENDIZAJE", 14, 5.5, { align: "center" });
+
 
  // Logo
- if (insigniaa) {
-   doc.addImage(insigniaa, "PNG", 22, 4, 4, 4);
- }
+ if (zorroImage) {
+  doc.addImage(zorroImage, "JPEG", 18, 5, 12, 8);
+  }
 
- // Texto del certificado
- doc.setFont("times", "bold");
- doc.setFontSize(18);
- doc.text("Con No. Documento 1.001.000.000 'Por haber completado exitosamente el curso' ", 14, 13.0, { align: "center" });
+  // Texto del certificado
+  doc.setFont("times", "normal");
+  doc.setFontSize(20);
+  doc.text(`Con No. Documento. ${documentNumber}`, 14, 14.0, { align: "center" });
 
- // Nombre del usuario
- doc.setFontSize(65);
- doc.setFont("Arial", "normal");
- doc.text(`${username}`, 14, 11, { align: "center" });
+  // Nombre del usuario
+  doc.setFontSize(45);
+  doc.setFont("times", "bold");
+  doc.text(`${username}`, 14, 10, { align: "center" });
 
- // Línea decorativa
- doc.setLineWidth(0.1);
- doc.setDrawColor(0, 0, 0);
- doc.line(8, 12, 20, 12 );
- // linea entre texto
- doc.setLineWidth(0.1);
- doc.setDrawColor(0, 0, 0);
- doc.line(8, 16, 20, 16);
- // 3 Línea decorativa al lado de la dorada
- doc.setLineWidth(0.1);
- doc.setDrawColor(0, 0, 0);
- doc.line(8.8, 0.9, 32, 0.9);
- // 4 linea decorativa
- doc.setLineWidth(0.1);
- doc.setDrawColor(255, 225, 180);
- doc.line(8.8, 1, 28, 1);
- // Línea decorativa dorada y más gruesa 
-  doc.setLineWidth(-0.5);  // Aumenta el grosor de la línea
-  doc.setDrawColor(255, 215, 0);  // Color dorado (RGB)
-  doc.line(8.8, 0.8, 32, 0.8);  // Dibuja la línea
-
-  // 3 Línea decorativa al lado de la dorada (negra, delgada y vertical)
+  // Línea decorativa
   doc.setLineWidth(0.1);
-  doc.setDrawColor(0, 0, 0);  // Color negro
-  doc.line(1.5, 14.5, 1.5, 24);  // Línea vertical (cambia las coordenadas Y para la altura)
-
-  // 4 Línea decorativa (naranja claro y vertical)
-  doc.setLineWidth(0.1);  // Grosor de línea delgada
-  doc.setDrawColor(255, 225, 180);  // Color naranja claro
-  doc.line(1.6, 14.5, 1.6, 24);  // Línea vertical
-
-  // Línea decorativa dorada y más gruesa (vertical)
-  doc.setLineWidth(0.1);  // Aumenta el grosor de la línea
-  doc.setDrawColor(255, 215, 0);  // Color dorado (RGB)
-  doc.line(1.4, 14.5, 1.4, 24);  // Línea vertical (con más grosor)
-
+  doc.setDrawColor(0, 0, 0);
+  doc.line(6, 11, 22, 11);
 
   // Texto descriptivo
-  doc.setFont("times", "bold");
-  doc.setFontSize(14);
+  doc.setFont("times", "normal");
+  doc.setFontSize(20);
   doc.text(
-    `"${courseTitle}"
-      Agradecemos tu dedicación y esfuerzo. ¡Continúa aprendiendo y alcanzando nuevas metas!
-      21 de Noviembre de 2024`,
-    14,14,
+    `Por completar exitosamente el curso "${courseTitle}". `,
+    14,
+    15.0,
     { align: "center" }
   );
- 
-  // Pie de página
-  doc.setTextColor(192, 192, 192);
-  doc.text("Este certificado fue generado automáticamente.", 14, 19.5, {
+
+  doc.setFontSize(20);
+  doc.text("Gracias por tu dedicación y esfuerzo. ¡Sigue aprendiendo y mejorando!", 14, 16.0, {
     align: "center",
   });
 
@@ -134,13 +108,13 @@ export const generateCertificatePreview = async (username, documentNumber, cours
     // Título principal
     doc.setFont("times", "bold");
     doc.setTextColor(0, 0, 0);
-    doc.setFontSize(35);
+    doc.setFontSize(25);
     doc.text("BRIGHTMIND", 14, 4.5, { align: "center" });
 
-    // Título principal
+    // Título segundario
     doc.setFont("times", "bold");
     doc.setTextColor(0, 0, 0);
-    doc.setFontSize(35);
+    doc.setFontSize(25);
     doc.text("CONSTANCIA DE APRENDIZAJE", 14, 5.5, { align: "center" });
 
 
@@ -150,32 +124,32 @@ export const generateCertificatePreview = async (username, documentNumber, cours
     }
 
     // Texto del certificado
-    doc.setFont("times", "bold");
-    doc.setFontSize(18);
-    doc.text(`Con No. Documento. ${documentNumber}`, 14, 13.0, { align: "center" });
+    doc.setFont("times", "normal");
+    doc.setFontSize(20);
+    doc.text(`Con No. Documento. ${documentNumber}`, 14, 14.0, { align: "center" });
 
     // Nombre del usuario
-    doc.setFontSize(55);
+    doc.setFontSize(45);
     doc.setFont("times", "bold");
-    doc.text(`${username}`, 14, 9.5, { align: "center" });
+    doc.text(`${username}`, 14, 10, { align: "center" });
 
     // Línea decorativa
     doc.setLineWidth(0.1);
     doc.setDrawColor(0, 0, 0);
-    doc.line(6, 10, 22, 10);
+    doc.line(6, 11, 22, 11);
 
     // Texto descriptivo
     doc.setFont("times", "normal");
-    doc.setFontSize(14);
+    doc.setFontSize(20);
     doc.text(
       `Por completar exitosamente el curso "${courseTitle}". `,
       14,
-      17.5,
+      15.0,
       { align: "center" }
     );
 
-    doc.setFontSize(14);
-    doc.text("Gracias por tu dedicación y esfuerzo. ¡Sigue aprendiendo y mejorando!", 14, 18.0, {
+    doc.setFontSize(20);
+    doc.text("Gracias por tu dedicación y esfuerzo. ¡Sigue aprendiendo y mejorando!", 14, 16.0, {
       align: "center",
     });
 
